bd00e95dc38a20d086a3f4def2af50b7237da555
EC2 Default User <ec2-user@ip-172-31-33-112.us-east-2.compute.internal>
2020-12-02 14:08:37 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  
+  include SessionHelper
+    
+  private
+  
+  def require_user_logged_in 
+      unless logged_in?
+          redirect_to login_url
+      end
+  end
+end 
